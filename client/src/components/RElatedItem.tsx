09a693ad923cd4e412dbdca3936917a9b7b6b0a1
Hammad Raza <01hammadraza@gmail.com>
2025-02-20 13:47:36 -0800

    'here'

--- client/src/components/RElatedItem.tsx
+++ client/src/components/RElatedItem.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import CardUi from './CardUi'
 
-export default function RElatedItem(data) {
+export default function RElatedItem({data}) {
   return (
     <>
       <CardUi card={data}/>
