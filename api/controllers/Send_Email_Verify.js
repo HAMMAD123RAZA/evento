6be729ff828fc054fbe2c9819fd88068267bcdc6
Hammad Raza <01hammadraza@gmail.com>
2025-03-01 07:37:41 -0800

    'e

--- api/controllers/Send_Email_Verify.js
+++ api/controllers/Send_Email_Verify.js
@@ -1,31 +1,55 @@
 import nodemailer from 'nodemailer';
+import Crypto from 'crypto-js';
+
 export const sendEmailVerify=async(req,res)=>{
-    const {emailData}=req.body;
-    const transporter=nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-    user: "01hammadraza@gmail.com",
-    pass:'wsqs mwzw sacu liih'
-        }
-    })
+try {
+    const {email,userId}=req.body
+
+    if (!email || !userId) {
+        res.status(400).json({message:'userId and Email required',success:false})
+    }
+
+    const token=crypto.randomBytes(32).toString('hex')
+    const expireTime=new Date();
+    expireTime.setHours(expireTime.getHours()+24)
+
+    const sql = neon('postgresql://neondb_owner:npg_bZvVMjNr87DE@ep-purple-hall-a8y9hegq-pooler.eastus2.azure.neon.tech/neondb?sslmode=require');
     
-    const mailOptions={
-        from:'01hammadraza@gmail.com',
-        to: '01hammadraza@gmail.com',
-        subject:`Event subscribed :${emailData?.title}`,
-        html:`
-              <h2>${emailData?.title}</h2>
-          <p><strong>Date:</strong> ${emailData?.date}</p>
-          <p><strong>Time:</strong> ${emailData?.time}</p>
-          <p><strong>Venue:</strong> ${emailData?.venue}</p>
-          <p><strong>Description:</strong> ${emailData?.description}</p>
-        `
-    }
-    try {
-        const info=await transporter.sendMail(mailOptions)
-        res.status(200).json({message:'email is sent successfully',info})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: "Failed to send email" });
-    }
-    }
+    const data=await sql`UPDATE user SET VerificationToken=${token},tokenExpires=${expireTime},isVerified=${false}`
+
+        const verificationUrl = `http://localhost:8080/verify-email?userId=${userId}&token=${token} &expireTime=${expireTime}`;
+
+        const transporter=nodemailer.createTransport({
+            service:'gmail',
+            auth:{
+                user:'01hammadraza@gmail.com',
+                pass: 'wsqs mwzw sacu liih'
+            }
+        })
+
+        const mailOptions={
+            from:'01hammadraza@gmail.com',
+            to:email,
+            subject:'Email Verification Link',
+            html:`
+                <h2>Verify Your Email</h2>
+                <p>Thank you for registering! Please verify your email by clicking the link below:</p>
+                <a href="${verificationUrl}"  >Click to Verify</a>
+                <p>This link will expire in 24 hours.</p>
+                <p>If you did not register for this account, please ignore this email.</p>
+            `
+        }
+
+        const sendMail=await transporter.sendMail(mailOptions)
+        res.status(200).json({
+            message: 'Verification email sent successfully',
+            success:true,
+            sendMail
+        })
+
+
+} catch (error) {
+    res.status(500).json({message:"error in sending email",success:false,error:error.message})
+    console.log('error in sending email',error)
+}
+}
